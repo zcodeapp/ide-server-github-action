9315517bfe89f0d6785f36d7508ad788eaca581c
João Moraes <joaomoraesbr@gmail.com>
2023-05-28 06:41:10 -0700

    fix default values

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,24 +4,8 @@ set -e
 set -o errexit
 set -o nounset
 
-IDE_SERVER_VERSION=$1
-IDE_SERVER_PORT=$2
-
-if [ -z "$IDE_SERVER_VERSION" ]; then
-  echo ""
-  echo "Missing IDE Server version in the [ide-server-version] input. Received value: $IDE_SERVER_VERSION"
-  echo ""
-
-  exit 2
-fi
-
-if [ -z "$IDE_SERVER_PORT" ]; then
-  echo ""
-  echo "Missing IDE Server port in the [ide-server-port] input. Received value: $IDE_SERVER_PORT"
-  echo ""
-
-  exit 2
-fi
+IDE_SERVER_VERSION=$1 || "latest"
+IDE_SERVER_PORT=$2 || "4000"
 
 docker run --detach --publish=$IDE_SERVER_PORT:4000 --name=test_container zcodeapp/ide-server:$IDE_SERVER_VERSION
 sleep 5 # wait container start
